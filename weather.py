e9b4921d726ef200a5d50788c74a24f4002ead1a
Thomas Biondi <tcb72@cornell.edu>
2019-09-29 14:00:02 -0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python3
+
+import os
+import time
+import re
+import slack
+import api_keys
+
+
+# get app token 
+token = api_keys.SLACK_TOKEN
 
 #importing requests to use API
 import requests
@@ -7,10 +16,10 @@ import requests
 website = "https://api.weather.gov/points/44.1333,-123.2144"
 
 #Forcing json format
-weather_data = (requests.get(website)).json()
+weather_data = requests.get(website).json()
 
 #Moving to current forecast API from the initial coordinate API
-eugene_weather = (requests.get(weather_data['properties']['forecast'])).json()
+eugene_weather = requests.get(weather_data['properties']['forecast']).json()
 
 #These are less detailed weather descriptors
 # temp = (eugene_weather['properties']['periods'][0]['temperature'])
@@ -20,5 +29,17 @@ eugene_weather = (requests.get(weather_data['properties']['forecast'])).json()
 #This gives a nice detailed summation
 detailed_forecast = eugene_weather['properties']['periods'][0]['detailedForecast']
 
-#std out giving forecast
-print(f"The current forecast for Eugene, OR is: {detailed_forecast}")
+
+# access slack web client
+client = slack.WebClient(token=token)
+
+# post message in the test channel
+# channel ids are indicated by string in URL 
+
+while True:
+    if int(time.time()) == 1569790740:
+        client.chat_postMessage(
+      channel="GNNRVLMLZ",
+      text= '\n :cloud: :cloud: :cloud: Good morning! Here\'s todays weather for Eugene... :cloud: :cloud: :cloud:\n'+ '```'  +         detailed_forecast + '```'
+    )
+    break
